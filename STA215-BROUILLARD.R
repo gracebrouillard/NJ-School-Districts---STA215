7cab0ea166b61dbe81c170faa08d131cdb36e28e
gracebrouillard <brouilg1@tcnj.edu>
2024-12-16 17:23:20 -0500

    Add files via upload

--- STA215-BROUILLARD.R
+++ STA215-BROUILLARD.R
@@ -5,6 +5,9 @@ library(dplyr)
 library(ggplot2)
 library(haven)
 library(psych)
+library(ggplot2)
+library(forcats)
+library(tidyselect)
 
 # set working directory
 setwd("~/Desktop/STA215")
@@ -12,10 +15,11 @@ setwd("~/Desktop/STA215")
 # Load data 
 data <- read.csv("raw_data.csv")
 
-#create table 2 contingency table
+#create table 2: contingency table
 table(data$funding, data$salary)
+chisq.test(table(data$funding, data$salary))
 
-#create table 1 descriptive statistics 
+#create table 1: descriptive statistics 
 table(data$population)
 describe(data$population)
 
@@ -23,56 +27,46 @@ table(data$gradiation)
 describe(data$graduation)
 
 table(data$funding)
-mean(data$funding)
+describe(data$funding)
 
 table(data$salary)
+describe(data$salary)
 
 
 #create figure 1 - boxplot 
-lm(funding ~ salary, data = data)
-aov(funding ~ salary, data = data)
-summary(funding ~ salary, data = data)
-boxplot(funding ~ salary, data = data)
+lm(salary ~ graduation, data = data)
+aov(salary ~ graduation, data = data)
+summary(salary ~ graduation, data = data)
+boxplot(salary ~ graduation, data = data)
+
 
 lm(funding ~ graduation, data = data)
 aov(funding ~ graduation, data = data)
 summary(funding ~ graduation, data = data)
+boxplot(funding ~ graduation, data = data)
+
 
 #create figure 2 - scatter plot
-linear_plot <- plot(data$graduation, data$funding)
+linear_plot <- plot(data$population, data$graduation)
 print(linear_plot)
-meany <- mean(data$graduation)
-meanx <- mean(data$funding)
-abline(h = meanx, col = "black")
-abline(v = meany, col = "black")
-linear_relationship <- lm(funding ~ graduation, data = data)
+#add x line and y line for means 
+meany <- mean(data$graduation, na.rm = TRUE)
+meanx <- mean(data$population, na.rm = TRUE)
+
+abline(v = meanx, col = "black")
+abline(h = meany, col = "black")
+
+linear_relationship <- lm(population ~ graduation, data = data)
 summary(linear_relationship)
 abline(linear_relationship, col = "red")
 
-linear_plot <- plot(data$population, data$funding)
-print(linear_plot)
-meany <- mean(data$population)
-meanx <- mean(data$funding)
-abline(h = meanx, col = "black")
-abline(v = meany, col = "black")
-linear_relationship <- lm(funding~ population, data = data)
-summary(linear_relationship)
-abline(linear_relationship, col = "red")
-
-#create figure 3 - residual plot 
-linear_plot <- plot(data$population, data$gradiation)
-print(linear_plot)
-
-# add x line and y line for means
-meany <- mean(data$population)
-meanx <- mean(data$graduation)
-
-linear_relationship <- lm(graduation ~ population, data = data)
-summary(linear_relationship)
-
+#create figure 3 - residual plot
 # Plot the residuals
-plot(data$graduation, residuals(linear_plot))
+plot(data$graduation, na.rm = TRUE, residuals(linear_plot))
 plot(data$population, residuals(linear_plot))
 
 # Add a horizontal line at zero to indicate the baseline
 abline(h = 0, col = "red")
+hist(data$population)
+
+
